48b1c6df4e0f25a5a71d0f5545b31c5ed10f183b
huangkai <h1770360848@outlook.com>
2024-10-17 21:06:48 +0800

    test: avoid open running database

--- db_test.go
+++ db_test.go
@@ -13,12 +13,26 @@ func TestOpen(t *testing.T) {
 	t.Run("should succeed without error", func(t *testing.T) {
 		t.Run("with default option", func(t *testing.T) {
 			tmpDir := t.TempDir()
-
 			dataDir := filepath.Join(tmpDir, "librakv")
+
 			db, err := librakv.Open(dataDir)
 
 			require.NoError(t, err)
 			require.NotNil(t, db)
 		})
+	})
+
+	t.Run("should error when another process is running", func(t *testing.T) {
+		tmpDir := t.TempDir()
+		dataDir := filepath.Join(tmpDir, "librakv")
+		db1, err := librakv.Open(dataDir)
+
+		require.NoError(t, err)
+		require.NotNil(t, db1)
+
+		db2, err := librakv.Open(dataDir)
+
+		require.Error(t, err)
+		require.Nil(t, db2)
 	})
 }
