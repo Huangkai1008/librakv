5a1868abaa433e786ba24a375f34e767f34fc0d6
huangkai <h1770360848@outlook.com>
2024-10-18 18:29:41 +0800

    test: open database with options

--- db_test.go
+++ db_test.go
@@ -10,8 +10,8 @@ import (
 )
 
 func TestOpen(t *testing.T) {
-	t.Run("should succeed without error", func(t *testing.T) {
-		t.Run("with default option", func(t *testing.T) {
+	t.Run("should succeed", func(t *testing.T) {
+		t.Run("with default options", func(t *testing.T) {
 			tmpDir := t.TempDir()
 			dataDir := filepath.Join(tmpDir, "librakv")
 
@@ -20,6 +20,37 @@ func TestOpen(t *testing.T) {
 			require.NoError(t, err)
 			require.NotNil(t, db)
 		})
+
+		t.Run("with options", func(t *testing.T) {
+			tmpDir := t.TempDir()
+			dataDir := filepath.Join(tmpDir, "librakv")
+
+			db, err := librakv.Open(dataDir, librakv.WithLogFileThresholdSize(1<<20))
+
+			require.NoError(t, err)
+			require.NotNil(t, db)
+		})
+	})
+
+	t.Run("should error when wrong options", func(t *testing.T) {
+		tmpDir := t.TempDir()
+		dataDir := filepath.Join(tmpDir, "librakv")
+
+		var tests = []struct {
+			name string
+			opts []librakv.Option
+		}{
+			{"zero log file threshold size", []librakv.Option{librakv.WithLogFileThresholdSize(0)}},
+		}
+
+		for _, tt := range tests {
+			t.Run(tt.name, func(t *testing.T) {
+				db, err := librakv.Open(dataDir, tt.opts...)
+
+				require.Nil(t, db)
+				require.Error(t, err)
+			})
+		}
 	})
 
 	t.Run("should error when another process is running", func(t *testing.T) {
